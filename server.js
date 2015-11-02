c9fb3bf5020f27190c3fb2a20c6f0586b353ca6f
chiefsmurph <chiefsmurph@gmail.com>
2015-11-02 15:27:26 -0800

    tracking

--- server.js
+++ server.js
@@ -398,7 +398,7 @@ io.on('connection', function(socket) {
 
     socket.on('sendClick', function(data) {
       var nowTime = Date.now();
-      if (lastClickTime - nowTime > 300) {
+      if (nowTime - lastClickTime > 300) {
         lastClickTime = nowTime;
         console.log('player played ' + data.play);
         sendToOpp('receiveClick', data);
