3775ad83b9964693a222a23a83845e009791a9f3
John Murphy <chiefsmurph@gmail.com>
2015-11-02 16:59:41 -0800

    this

--- server.js
+++ server.js
@@ -398,7 +398,7 @@ io.on('connection', function(socket) {
 
     socket.on('sendClick', function(data) {
       var nowTime = Date.now();
-      if (nowTime - lastClickTime > 500) {
+      if (nowTime - lastClickTime > 50) {
         lastClickTime = nowTime;
         console.log('player played ' + data.play);
         sendToOpp('receiveClick', data);
