abf6723223139a4dd9192705865274e12b066cdb
John Murphy <chiefsmurph@gmail.com>
2015-11-12 16:34:51 -0800

    up to date

--- public/app.jsx
+++ public/app.jsx
@@ -15,7 +15,7 @@ require('./js/odometer-0.4.6/themes/odometer-theme-minimal.css');
 var infoIconPNG = require('./img/info_icon2.png');
 var winnersIconPNG = require('./img/crown.png');
 
-var bob = require('./img/bob.png');
+var bob = require('./img/newbob.png');
 var docCookies = require('./js/mozilla-cookies.js');
 require('./js/odometer-0.4.6/odometer.min.js');
 
